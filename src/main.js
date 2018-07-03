5c0370e10feabdf8e8d7170c20bf4a15cd78f0c8
alppacat <lorena.pratts@gmail.com>
2018-07-02 22:29:08 -0500

    Cambio a interfaz, impresión de nombre de las alumnas por generación y sede, hace falta impresión general por generación

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//Todo lo que se muestre en pantalla va en este archivo
+// Todo lo que se muestre en pantalla va en este archivo
 let primerCaja = document.querySelector("#inputs"); //Trayendo caja de inputs de HTML
 let botonEntrar = document.getElementById("entrar"); //id boton entrar
 
@@ -42,16 +42,7 @@ const iterator = (data) => {
 
     const estLimaGen3 = data[lima].generacion["tercera"].estudiantes;
     const estLimaGen4 = data[lima].generacion["cuarta"].estudiantes;
-    // function () {
-    //  data[lima].generacion["cuarta"].estudiantes;
-    // estLimaGen4.forEach((limaGen4)=> {
-    // const limaGen4Nom = limaGen4.nombre
-    //   })
-
     const estLimaGen5 = data[lima].generacion["quinta"].estudiantes;
-    estLimaGen5.forEach((limaGen5)=> {
-    const limaGen5Nom = limaGen5.nombre
-    });
 //mexico
     const estMexGen3 = data[mexico].generacion["tercera"].estudiantes;
     const estMexGen4 = data[mexico].generacion["cuarta"].estudiantes;
@@ -60,6 +51,7 @@ const iterator = (data) => {
     const estSanGen3 = data[santiago].generacion["tercera"].estudiantes;
     const estSanGen4 = data[santiago].generacion["cuarta"].estudiantes;
     const estSanGen5 = data[santiago].generacion["quinta"].estudiantes;
+
 
 // Se rota la sede y se agrega con el DOM
 //   const selector = document.getElementById("opcion");
@@ -70,36 +62,121 @@ const iterator = (data) => {
 //     selector.appendChild(option);
 //
 // })
-    const botonGo = document.getElementById("gobutton");
 
     selector.addEventListener("change", iterator => {
   // Condicionales según la opción del selector
       if (selector.options[0].selected == true ) {
-         data[lima].generacion["cuarta"].estudiantes;
-        estLimaGen4.forEach((limaGen4)=> {
-        const limaGen4Nom = limaGen4.nombre;
-        const estuBox = document.createElement("div");
+        estLimaGen3.forEach((limaGen3)=> {
+        const limaGen3Nom = limaGen3.nombre;
+        // const progreso = Object.keys(limaGen3);
+        // const porcentaje = progreso[2].porcentajeCompletado;
+        // // const completado = porcentaje.porcentajeCompletado;
+        // // console.log(typeof(porcentaje));
+        // console.log(porcentaje);
+        const root = document.getElementById("root");
+        const subtitulo = document.getElementById("subtitulo");
+        const estuBox = document.createElement("p");
         estuBox.setAttribute("class","estuBox");
-        estuBox.innerHTML = limaGen4Nom;
+        subtitulo.innerHTML = "Nombre de las alumnas"
+        estuBox.innerHTML = limaGen3Nom + "  " + limaGen3Nom.progreso;
         root.appendChild(estuBox);
-          })
-        // const sedesBox = document.createElement("div");
-        // sedesBox.setAttribute("class","sedesBox");
-        // sedesBox.innerHTML = estLimaGen5;
-        // root.appendChild(sedesBox);
+            })
 
     console.log("lima");
 
           }else if (selector.options[1].selected == true) {
-              console.log("mexico");
+            estLimaGen3.forEach((limaGen4)=> {
+            const limaGen4Nom = limaGen4.nombre;
+            const root = document.getElementById("root");
+            const subtitulo = document.getElementById("subtitulo");
+            const estuBox = document.createElement("p");
+            estuBox.setAttribute("class","estuBox");
+            subtitulo.innerHTML = "Nombre de las alumnas";
+            estuBox.innerHTML = limaGen4Nom + "  " + limaGen4Nom.progreso;
+            root.appendChild(estuBox);
+                })
+
               }else if (selector.options[2].selected == true) {
-  console.log("santiago");
-      }
+                estLimaGen5.forEach((limaGen5)=> {
+                const limaGen5Nom = limaGen5.nombre;
+                const root = document.getElementById("root");
+                const subtitulo = document.getElementById("subtitulo");
+                const estuBox = document.createElement("p");
+                estuBox.setAttribute("class","estuBox");
+                subtitulo.innerHTML = "Nombre de las alumnas";
+                estuBox.innerHTML = limaGen5Nom + "  " + limaGen5Nom.progreso;
+                root.appendChild(estuBox);
+                    })
+                  }else if (selector.options[3].selected == true) {
+                    estMexGen3.forEach((mexGen3)=> {
+                    const mexGen3Nom = mexGen3.nombre;
+                    const root = document.getElementById("root");
+                    const subtitulo = document.getElementById("subtitulo");
+                    const estuBox = document.createElement("p");
+                    estuBox.setAttribute("class","estuBox");
+                    subtitulo.innerHTML = "Nombre de las alumnas";
+                    estuBox.innerHTML = mexGen3Nom + "  " + mexGen3Nom.progreso;
+                    root.appendChild(estuBox);
+                        })
+                      }else if (selector.options[4].selected == true) {
+                        estMexGen4.forEach((mexGen4)=> {
+                        const mexGen4Nom = mexGen4.nombre;
+                        const root = document.getElementById("root");
+                        const subtitulo = document.getElementById("subtitulo");
+                        const estuBox = document.createElement("p");
+                        estuBox.setAttribute("class","estuBox");
+                        subtitulo.innerHTML = "Nombre de las alumnas";
+                        estuBox.innerHTML = mexGen4Nom + "  " + mexGen4Nom.progreso;
+                        root.appendChild(estuBox);
+                            })
+                          }else if (selector.options[5].selected == true) {
+                            estMexGen5.forEach((mexGen5)=> {
+                            const mexGen5Nom = mexGen5.nombre;
+                            const root = document.getElementById("root");
+                            const subtitulo = document.getElementById("subtitulo");
+                            const estuBox = document.createElement("p");
+                            estuBox.setAttribute("class","estuBox");
+                            subtitulo.innerHTML = "Nombre de las alumnas";
+                            estuBox.innerHTML = mexGen5Nom + "  " + mexGen5Nom.progreso;
+                            root.appendChild(estuBox);
+                                })
+                              }else if (selector.options[6].selected == true) {
+                                estSanGen3.forEach((sanGen3)=> {
+                                const sanGen3Nom = sanGen3.nombre;
+                                const root = document.getElementById("root");
+                                const subtitulo = document.getElementById("subtitulo");
+                                const estuBox = document.createElement("p");
+                                estuBox.setAttribute("class","estuBox");
+                                subtitulo.innerHTML = "Nombre de las alumnas";
+                                estuBox.innerHTML = sanGen3Nom + "  " + sanGen3Nom.progreso;
+                                root.appendChild(estuBox);
+                                    })
+                                  }else if (selector.options[7].selected == true) {
+                                    estSanGen4.forEach((sanGen4)=> {
+                                    const sanGen4Nom = sanGen4.nombre;
+                                    const root = document.getElementById("root");
+                                    const subtitulo = document.getElementById("subtitulo");
+                                    const estuBox = document.createElement("p");
+                                    estuBox.setAttribute("class","estuBox");
+                                    subtitulo.innerHTML = "Nombre de las alumnas";
+                                    estuBox.innerHTML = sanGen4Nom + "  " + sanGen4Nom.progreso;
+                                    root.appendChild(estuBox);
+                                        })
+                                      }else if (selector.options[8].selected == true) {
+                                        estSanGen5.forEach((sanGen5)=> {
+                                        const sanGen5Nom = sanGen5.nombre;
+                                        const root = document.getElementById("root");
+                                        const subtitulo = document.getElementById("subtitulo");
+                                        const estuBox = document.createElement("p");
+                                        estuBox.setAttribute("class","estuBox");
+                                        subtitulo.innerHTML = "Nombre de las alumnas";
+                                        estuBox.innerHTML = sanGen5Nom + "  " + sanGen5Nom.progreso;
+                                        root.appendChild(estuBox);
+                                            })
+
+}
 
     })
-
-
-// Entrando a las generaciones
 
 
 
