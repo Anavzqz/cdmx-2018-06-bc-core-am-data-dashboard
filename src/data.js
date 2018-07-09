fd8896e51b9a51d72767d885f2c918cfd58e65e5
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-07-09 10:50:29 -0500

    Agregando Responsive

--- src/data.js
+++ src/data.js
@@ -1,28 +1,12 @@
-window.onload = () => { //Carga la data al abirir la página
-  const url = 'https://api.myjson.com/bins/nslqe'; //Jalar URL
-  const pullData = () => {
-    fetch(url).then(response => response.json())
-      .then(laboratoria => { //Asignando nombre a la información
-        drawCampus(laboratoria);
-        const pullComputeGeneration = computeGenerationsStats(laboratoria); //Enviando data a Generation e invoco funcion
-        console.log(pullComputeGeneration);
-        const pullComputeStudent = computeStudentsStats(laboratoria); //Enviar data a Students e invoco funcion
-        console.log(pullComputeStudent);
+window.data = { // Carga la data al abirir la página
 
-      })
-      .catch(error => {
-        console.log("Error");
-      })
-  }
-  pullData();
-
-  computeStudentsStats = (laboratoria) => { //Función lista de estudiantes
+  computeStudentsStats: (laboratoria) => { // Función lista de estudiantes
     const student = [];
-    let newArray = []; //Variables con nombres acorde al readme
-    let name = "";
-    let email = "";
-    let campus = "";
-    let generation = "";
+    let newArray = []; // Variables con nombres acorde al readme
+    let name = '';
+    let email = '';
+    let campus = '';
+    let generation = '';
     let stats = [];
     let status = 0;
     let completedPercentage = 0;
@@ -32,15 +16,17 @@ window.onload = () => { //Carga la data al abirir la página
     let percentageDuration1 = 0;
     let subtopics = [];
     let completedPercentage3 = 0;
-    let type = "";
+    let type = '';
     let duration = 0;
-    for (key in laboratoria) { //Recorriendo las propiedades del objeto
-      campus = key.toUpperCase(); //Obteniendo sede y volviendola mayúsculas
-      const generations = Object.keys(laboratoria[key].generacion); //Devolviendo las propiedades del objeto generacion
+
+    for (key in laboratoria) { // Recorriendo las propiedades del objeto
+      campus = key.toUpperCase(); // Obteniendo sede y volviendola mayúsculas
+      const generations = Object.keys(laboratoria[key].generacion); // Devolviendo las propiedades del objeto generacion
+
       generations.map((generation) => {
         generation = generation;
-        const students = laboratoria[key].generacion[generation].estudiantes; //Devolviendo las propiedades del objeto estudiante
-        newArray = students.map((student, i, array) => { //Invocando indices para nuevo array
+        const students = laboratoria[key].generacion[generation].estudiantes; // Devolviendo las propiedades del objeto estudiante
+        newArray = students.map((student, i, array) => { // Invocando indices para nuevo array
           name = array[i].nombre;
           email = array[i].correo;
           completedPercentage = array[i].progreso.porcentajeCompletado;
@@ -59,69 +45,71 @@ window.onload = () => { //Carga la data al abirir la página
           // for(topics in laboratoria)
 
 
-
           return {
-            "name": name,
-            "email": email,
-            "campus": campus,
-            "generation": generation.toUpperCase(), //Volviendo mayúsculas 
+            'name': name,
+            'email': email,
+            'campus': campus,
+            'generation': generation.toUpperCase(), // Volviendo mayúsculas
             stats: {
-              "status": status,
-              "completedPercentage": completedPercentage,
-              "percentageDuration": percentageDuration,
+              'status': status,
+              'completedPercentage': completedPercentage,
+              'percentageDuration': percentageDuration,
               topics: {
-                "completePercentage": completedPercentage1,
-                "percentageDuration": percentageDuration1,
+                'completePercentage': completedPercentage1,
+                'percentageDuration': percentageDuration1,
                 subtopics: {
-                  "completePercentage": completedPercentage3,
-                  "type": type,
-                  "duration": duration
+                  'completePercentage': completedPercentage3,
+                  'type': type,
+                  'duration': duration
 
                 }
               }
             }
-          }
-        })
-        student.push(newArray); //Agregando elementos al array
-      })
+          };
+        });
+        student.push(newArray); // Agregando elementos al array
+      });
     }
     return student;
-  }
+  },
 
-  computeGenerationsStats = (laboratoria) => { //Función generación
+  computeGenerationsStats: (laboratoria) => { // Función generación
     const countGen = [];
     let otherArray = [];
-    let campus = "";
-    let generation = "";
+    let campus = '';
+    let generation = '';
     let average = 0;
-    for (key in laboratoria) { //Recorriendo las propiedades del objeto
-      campus = key; //Sede
+
+    for (key in laboratoria) { // Recorriendo las propiedades del objeto
+      campus = key; // Sede
       average = 0;
       const generations = Object.keys(laboratoria[key].generacion);
+
       generations.map((generation) => {
         generation = generation;
+        // console.log(generation);
         const pupils = laboratoria[key].generacion[generation].estudiantes;
-        otherArray = pupils.map((pupil, i, array) => { //Map usa tres parámetros para recorrer el objeto por su indice y devolver un array
+        otherArray = pupils.map((pupil, i, array) => { // Map usa tres parámetros para recorrer el objeto por su indice y devolver un array
           average += array[i].progreso.porcentajeCompletado;
-          average = Math.round(average / array.length); //Redondeando promedio
-          return { //Creación del objeto
-            "campus": key.toUpperCase(), //Volviendo mayúsculas 
-            "generation": generation.toUpperCase(), //Volviendo mayúsculas 
-            "average": average,
-            "count": array.length
-          }
-        })
+          average = Math.round(average / array.length); // Redondeando promedio
+          return { // Creación del objeto
+            'campus': key.toUpperCase(), // Volviendo mayúsculas
+            'generation': generation.toUpperCase(), // Volviendo mayúsculas
+            'average': average,
+            'count': array.length
+          };
+        });
         countGen.push(otherArray);
-      })
+      });
     }
     return countGen;
+  },
+
+  sortStudens: (laboratoria) => {
+
+  },
+
+  filterStudents: (laboratoria) => {
+
   }
-
-  sortStudens = (laboratoria) => {
-
-  }
-
-  filterStudents = (laboratoria) => {
-
-  }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,39 +1,120 @@
 // Todo lo que se muestre en pantalla va en este archivo
-let primerCaja = document.querySelector("#inputs"); //Trayendo caja de inputs de HTML
-let botonEntrar = document.getElementById("entrar"); //id boton entrar
+let primerCaja = document.querySelector('#inputs'); // Trayendo caja de inputs de HTML
+let botonEntrar = document.getElementById('entrar'); // id boton entrar
 
 
-entrar.addEventListener("click", event => { //Evento del botón login
-  let usuario = document.getElementById("usuario").value; //Valor de usuario
-  let clave = parseInt(document.getElementById("clave").value); //Valor de clave
-  if (usuario === "admin" && clave === 1234) { //Comparativo de simulación login
-    inputs.style.display = "none"; //Se esconde sección para el siguiente paso
-
+entrar.addEventListener('click', event => { // Evento del botón login
+  let usuario = document.getElementById('usuario').value; // Valor de usuario
+  let clave = parseInt(document.getElementById('clave').value); // Valor de clave
+  if (usuario === 'admin' && clave === 1234) { // Comparativo de simulación login
+    inputs.style.display = 'none'; // Se esconde sección para el siguiente paso
   } else {
-    alert("¡Verifica los datos ingresados!") //Alerta de falta de datos
+    alert('¡Verifica los datos ingresados!'); // Alerta de falta de datos
   };
-})
+});
 
+const url = 'https://api.myjson.com/bins/nslqe'; // Jalar URL
+const pullData = () => {
+  fetch(url).then(response => response.json())
+    .then(laboratoria => { // Asignando nombre a la información
+      drawCampus(laboratoria);
 
+      console.log(data.computeGenerationsStats(laboratoria));
+      // drawGeneration(laboratoria)
+      //       // console.log(totalStudents);
+      //       countStudents(laboratoria)
+      arr = data.computeGenerationsStats(laboratoria);
+      for (let i = 0; i < arr.length; i++) {
+        totalStudents = arr[i][0].count;
+        // console.log(totalStudents);
+        if (document.getElementById('selector').value === arr[i][0]) {
+          console.log('si sirve');
+        }
+      }
+    });
+  // .catch(error => {
+  //   console.log("Error");
+  // })
+};
+pullData();
 
+const drawCampus = (laboratoria) => { // Función para pintar en HTML
+  let campoRellenar = document.getElementById('selector');
+  let relleno = '';
 
-const drawCampus = (laboratoria) => { //Función para pintar en HTML
-  let campoRellenar = document.getElementById("selector");
-  let relleno = "";
-  for (key in laboratoria) { //Iterando en Data
-    relleno += `
-              <option>Sedes</option>
-              <optgroup label="${key.toUpperCase()}">
-              <option>Tercera</option>
-              <option>Cuarta</option>
-              <option>Quinta</option>
-            </optgroup>`
+  for (key in laboratoria) {
+    relleno +=// Iterando en Data
+    `<optgroup label="${key.charAt(0).toUpperCase() + key.slice(1)}">
+    </optgroup>
+    `;
+    let values = Object.keys(laboratoria[key].generacion);
+    // console.log(laboratoria[key].generacion);
+    let ent = Object.values(values);
+    // console.log(ent);
 
-    campoRellenar.innerHTML = relleno
-    campoRellenar.addEventListener("change", drawGeneration)
+    for (let i = 0; i < ent.length; i++) {
+      ent[i];
+
+      relleno += `
+              <option>${ent[i]}</option>
+            `;
+
+      campoRellenar.innerHTML = relleno;
+    }
   }
-}
+  campoRellenar.addEventListener('change', countStudents => {
+    arr = data.computeGenerationsStats(laboratoria);
+    let campoRellenar = document.getElementById('root');
+    let relleno = '';
+    // for (let i = 0; i < arr.length; i++) {
+    //   totalStudents = arr[i][0].count;
+    // console.log(arr);7
+    if (selector.options[1].selected === true) {
+      console.log(arr[1][1].count);
 
-const drawGeneration = (e) => {
-  console.log(selector[e.target.selectedIndex].innerHTML);
-}
+      relleno += `
+              <p>Promedio general: </p>
+              <p>Estudiantes totales: ${arr[1][1].count} </p>
+              `;
+
+      campoRellenar.innerHTML = relleno;
+    } else if (selector.options[1].selected === false) {
+      console.log(arr[0][0].count);
+
+      relleno += `
+            <p>Promedio general: </p>
+            <p>Estudiantes totales: ${arr[0][0].count} </p>
+
+      `;
+      campoRellenar.innerHTML = relleno;
+    }
+  });
+};
+
+// const drawGeneration = (e) => {
+//
+//   console.log(selector[e.target.selectedIndex].innerHTML);
+//   }
+
+
+// let seleccionIndex = document.getElementById("selector").selectedIndex;
+// for (key in laboratoria) {
+//   let values = Object.keys(laboratoria[key].generacion)
+//
+// const root = document.getElementById("root");
+// let totalArr = data.computeGenerationsStats(laboratoria);
+// for (let i = 0; i < totalArr.length; i++) {
+//   estudiantesTotales = totalArr[i].length;
+//   console.log(estudiantesTotales);
+//   if ( seleccionIndex == key) {
+//
+//     estudiantesTotales += `
+//             <p>${estudiantesTotales} </p>
+//     `
+//     root.innerHTML = estudiantesTotales
+//   }
+//   }
+// }
+
+
+// console.log(selector[e.target.selectedIndex].innerHTML);
